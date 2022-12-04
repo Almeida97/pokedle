bb55c6cc085b5827ce2e5cf59e2d7515e1e3d75e
Paulo Almeida <paulo1771997@gmail.com>
2022-12-04 15:18:10 +0000

    +Add Image fetch and display

--- pokedle/Service.swift
+++ pokedle/Service.swift
@@ -6,9 +6,11 @@
 //
 
 import Foundation
+import UIKit
 class Service {
-    
-    static func fetchPokemon(name: String) {
+
+    static func fetchPokemon(name: String, completion: @escaping (Pokemon)-> Void){
+        
         guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon/\(name)") else{
             return
         }
@@ -17,41 +19,92 @@ class Service {
                 return
             }
             do {
-                let pokemon = try JSONDecoder().decode(Pokemon.self, from: data)
-
+                let dataDecoded = try JSONDecoder().decode(Pokemon.self, from: data)
+                completion(dataDecoded)
             }catch {
                 print(error)
             }
+            
+        }
+        task.resume()
+    }
+    
+    static func fetchPokemonImage(url: String, completion: @escaping (Data)-> Void){
+        
+        guard let url = URL(string: url) else{
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) {data, response, error in
+            guard let data = data, error == nil else{
+                return
+            }
+            completion(data)
+            
         }
         task.resume()
     }
 }
 
 struct Pokemon: Codable {
-    
     var name: String
     var types: [Types]
     var sprites: Sprite
+    
+    var alltypesString: String{
+        var keep = ""
+        for type in types {
+            if !(keep.isEmpty) {
+                keep += "/\(type.type.name)"
+            }
+            keep.append(type.type.name)
+        }
+        return keep
+    }
+    
+    var typesArray: [String] {
+        var array: [String] = []
+        for type in types {
+            array.append(type.type.name)
+        }
+        return array
+    }
+    
+    init(name: String, types: [Types], sprites: Sprite, alltypesString: String, typesArray: [String]) {
+            self.name = name
+        self.types = types
+        self.sprites = sprites
+            
+        }
+    
 }
 
-struct Sprite: Codable{
-    
-    var back_default: String?
-    var back_female: String?
-    var back_shiny: String?
-    var back_shiny_female: String?
+struct Sprite: Codable {
     var front_default: String?
-    var front_female: String?
-    var front_shiny: String?
-    var front_shiny_female: String?
 }
 
 struct Types: Codable {
     var slot: Int
     var type: type
+    
 }
 
-struct type: Codable{
+struct type: Codable, Equatable {
     var name: String
     var url : String
 }
+
+
+struct Guess {
+    var pokemon : Pokemon?
+    var nameColor: UIColor
+    var typeColor: UIColor
+    var pokemonImage: Data?
+    
+    init(pokemon: Pokemon?, nameColor: UIColor, typeColor: UIColor, pokemonImage: Data?) {
+        self.pokemon = pokemon
+        self.nameColor = nameColor
+        self.typeColor = typeColor
+        self.pokemonImage = pokemonImage
+       }
+}
+
--- pokedle/ViewController.swift
+++ pokedle/ViewController.swift
@@ -7,57 +7,70 @@
 
 import UIKit
 
-struct Guess {
-    var pokeName: String?
-    var type: String?
-    var pokeNameBackgroundColor: UIColor?
-    var typeBackgroundColor: UIColor?
-    
-}
-
 class ViewController: UIViewController{
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var guessTextField: UITextField!
     @IBOutlet weak var guessButton: UIButton!
-    
-    let correctGuess = Guess(pokeName: "charmander", type: "Fire")
-    var pokemon: Pokemon?
-    var guesses: [Guess] = [] /* = [Guess(pokeName: "Pokemon", type: "Type(s)", pokeNameBackgroundColor: .gray, typeBackgroundColor: .gray)]*/
-    
-    @IBAction func guessButtonTapped(_ sender: Any) {
-        var userGuess = Guess(pokeName: guessTextField.text, type: "Fire")
-        
-        if userGuess.pokeName == correctGuess.pokeName {
-            userGuess.pokeNameBackgroundColor = .green
-            userGuess.typeBackgroundColor = .green
-            guesses.insert(userGuess, at: 0)
-        }else{
-            userGuess.pokeNameBackgroundColor = .red
-            if userGuess.type == correctGuess.type{
-                userGuess.typeBackgroundColor = .orange
-                
-            }else{
-                userGuess.typeBackgroundColor = .red
-            }
-            guesses.insert(userGuess, at: 0)
-        }
-        tableView.reloadData()
-    }
+
+    var correctPokemon: Pokemon?
+    var userPokemon: Pokemon?
+    var pokemonGuesses = [Guess]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.delegate = self
         self.tableView.dataSource = self
         self.tableView.register(UINib(nibName: "CustomHeaderView", bundle: nil), forHeaderFooterViewReuseIdentifier: "CustomHeaderView")
+        
+        // define correct answer hard coded for now
+        Service.fetchPokemon(name: "mew") { [weak self] pokemon in
+            self?.correctPokemon = pokemon
+        }
     }
+    
+    func checkPokemon() {
+        var userGuess = Guess(pokemon: nil, nameColor: .systemGray, typeColor: .systemGray, pokemonImage: nil)
+        if userPokemon?.name != correctPokemon?.name  {
 
+            if userPokemon?.types[0].type == correctPokemon?.types[0].type{
+                userGuess = Guess(pokemon: userPokemon, nameColor: .red, typeColor: .green, pokemonImage: nil)
+            }else{
+                userGuess = Guess(pokemon: userPokemon, nameColor: .red, typeColor: .red, pokemonImage: nil)
+            }
+        }else{
+            userGuess = Guess(pokemon: userPokemon, nameColor: .green, typeColor: .green, pokemonImage: nil)
+            let alert = UIAlertController(title: "You got that in \(pokemonGuesses.count+1)", message: "New Game", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "ok", style: .default,
+                                                  handler: nil))
+            DispatchQueue.main.async {
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+        Service.fetchPokemonImage(url: (self.userPokemon?.sprites.front_default)!) { imageData in
+            DispatchQueue.main.async { [weak self] in
+                userGuess.pokemonImage = imageData
+                self!.pokemonGuesses.insert(userGuess, at: 0)
+                self!.tableView.reloadData()
+            }
+        }
+    }
+    
+    @IBAction func guessButtonTapped(_ sender: Any) {
+        guard let pokemonText = guessTextField.text, guessTextField.text != nil else{
+            return
+        }
+        Service.fetchPokemon(name: pokemonText) { [weak self] pokemon in
+            self!.userPokemon = pokemon
+            self!.checkPokemon()
+            
+        }
+    }
 
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    
-   
+
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "CustomHeaderView") as! CustomHeader
         return headerView
@@ -68,7 +81,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return guesses.count
+        return pokemonGuesses.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -77,10 +90,11 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         cell.cellLabel.layer.masksToBounds = true
         cell.cellType.layer.cornerRadius = 8
         cell.cellLabel.layer.cornerRadius = 8
-        cell.cellLabel.text = guesses[indexPath.row].pokeName
-        cell.cellLabel.backgroundColor = guesses[indexPath.row].pokeNameBackgroundColor
-        cell.cellType.text = guesses[indexPath.row].type
-        cell.cellType.backgroundColor = guesses[indexPath.row].typeBackgroundColor
+        cell.cellImage.image = UIImage(data: pokemonGuesses[indexPath.row].pokemonImage!)
+        cell.cellLabel.text = pokemonGuesses[indexPath.row].pokemon?.name
+        cell.cellType.text = pokemonGuesses[indexPath.row].pokemon?.alltypesString
+        cell.cellType.backgroundColor = pokemonGuesses[indexPath.row].typeColor
+        cell.cellLabel.backgroundColor = pokemonGuesses[indexPath.row].nameColor
         return cell
     }
     
